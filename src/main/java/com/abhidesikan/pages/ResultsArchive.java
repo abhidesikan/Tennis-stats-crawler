f6fa5133660b6ff520dda57c2d5ac1569a944cdd
Abhishek Cumbakonam Desikan <abhidesikan@AMBP.attlocal.net>
2016-06-06 00:44:58 -0400

    Extracting fields, half done

---
+++ src/main/java/com/abhidesikan/pages/ResultsArchive.java
@@ -0,0 +1,83 @@
+package com.abhidesikan.pages;
+
+/**
+ * Created by abhidesikan on 6/3/16.
+ */
+
+
+public class ResultsArchive {
+
+    private String tournament;
+    private String[] draw = new String[2];
+    private String surface;
+    private String totalPrizeMoney;
+    private String[] winner = new String[2];
+    private int tournamentCode;
+    private String location;
+    private String date;
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(String tournament) {
+        this.tournament = tournament;
+    }
+
+    public String[] getDraw() {
+        return draw;
+    }
+
+    public void setDraw(String[] draw) {
+        this.draw = draw;
+    }
+
+    public String getSurface() {
+        return surface;
+    }
+
+    public void setSurface(String surface) {
+        this.surface = surface;
+    }
+
+    public String getTotalPrizeMoney() {
+        return totalPrizeMoney;
+    }
+
+    public void setTotalPrizeMoney(String totalPrizeMoney) {
+        this.totalPrizeMoney = totalPrizeMoney;
+    }
+
+    public String[] getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String[] winner) {
+        this.winner = winner;
+    }
+
+    public int getTournamentCode() {
+        return tournamentCode;
+    }
+
+    public void setTournamentCode(int tournamentCode) {
+        this.tournamentCode = tournamentCode;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
--- src/main/java/com/abhidesikan/tennisstatscrawler/TournamentInfoExtractor.java
+++ src/main/java/com/abhidesikan/tennisstatscrawler/TournamentInfoExtractor.java
@@ -1,5 +1,6 @@
 package com.abhidesikan.tennisstatscrawler;
 
+import com.abhidesikan.pages.ResultsArchive;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by abhidesikan on 5/11/16.
@@ -17,9 +20,10 @@ public class TournamentInfoExtractor {
     final Logger logger = LoggerFactory.getLogger(TournamentInfoExtractor.class);
 
     private String tournamentUrl = "http://www.atpworldtour.com/en/scores/results-archive?year=";
-
+    private List<ResultsArchive> resultsArchiveList = new ArrayList<ResultsArchive>();
 
     public void getTournamentInformationForYear(String year) {
+
         tournamentUrl = tournamentUrl.concat(year);
         try{
             Document doc = Jsoup.connect(tournamentUrl).get();
@@ -36,7 +40,20 @@ public class TournamentInfoExtractor {
                 logger.info("Displaying body elements:");
                 for(Element body: table.select("tbody")) {
                     for(Element tr: body.select("tr")) {
-                        System.out.println(tr.text());
+                        ResultsArchive resultsArchive = new ResultsArchive();
+                        resultsArchive.setTournament(tr.select("span[class = tourney-title]").text());
+                        resultsArchive.setDate(tr.select("span[class = tourney-dates]").text());
+                        resultsArchive.setLocation(tr.select("span[class = tourney-location]").text());
+                        String drawSpan = tr.select(".tourney-details> .info-area > .item-details > a[href]> span[class = item-value]").text();
+                        if(!drawSpan.isEmpty()) {
+                            resultsArchive.setDraw(new String[]{tr.select(".tourney-details> .info-area > .item-details > a[href]> span[class = item-value]").text().split(" ")[0],
+                                    tr.select(".tourney-details> .info-area > .item-details > a[href]> span[class = item-value]").text().split(" ")[1]});
+                        }
+                        resultsArchive.setTotalPrizeMoney(tr.select(".tourney-details.fin-commit > .info-area > .item-details > span[class = item-value]").text());
+                        System.out.println(resultsArchive.getTotalPrizeMoney());
+                        resultsArchive.setSurface(tr.select(".tourney-details > .info-area > .item-details").text());
+                        System.out.println(resultsArchive.getSurface());
+
                     }
                 }
             }
